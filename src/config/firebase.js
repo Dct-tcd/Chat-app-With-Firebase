0455bce601055695fefbb3e6f9e9fbaef6842f4e
Dev chaitanya tiwari <110164808+Dct-tcd@users.noreply.github.com>
2023-05-21 23:05:16 +0530

    removed project credentials

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -9,13 +9,7 @@ import { getStorage } from "firebase/storage";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyB9nCgRuDlOzob37IzvOpgpvuZYNrbmnOc",
-  authDomain: "chatter-37dc6.firebaseapp.com",
-  projectId: "chatter-37dc6",
-  storageBucket: "chatter-37dc6.appspot.com",
-  messagingSenderId: "931769986875",
-  appId: "1:931769986875:web:799b3ba5be12d778c4511e",
-  measurementId: "G-VBWB75YFP2"
+  
 };
 
 // Initialize Firebase
